d26c9274bd5f3dc8dac1698bb1c57a3d5c1f9fc6
andreasSchauer <andreas.schauer@yahoo.com>
2025-04-25 14:26:27 +0200

    set up cd workflow

--- json.go
+++ json.go
@@ -32,6 +32,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-		respondWithError(w, code, "couldn't write JSON response", err)
+		log.Printf("Critical error writing response: %s", err)
 	}
 }
--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func main() {
 	srv := &http.Server{
 		Addr:              ":" + port,
 		Handler:           router,
-		ReadHeaderTimeout: time.Second * 10,
+		ReadHeaderTimeout: time.Second * 5,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
